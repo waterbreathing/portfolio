c680351f26b5f6b9d067018776f985f0e050a779
Michal Dzik <xademizas@gmail.com>
2019-10-05 17:32:46 +0200

    Updates

---
+++ precache-manifest.541f920e2be4a92ad60d763f9049682d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3429091b1b756e02e1fdfebca512a30",
+    "url": "/portfolio/static/media/world2.d3429091.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2760e0b45d3668f916ea",
+    "url": "/portfolio/static/js/main.216207d4.chunk.js"
+  },
+  {
+    "revision": "a0296135bc231641639e",
+    "url": "/portfolio/static/js/2.3e4e0912.chunk.js"
+  },
+  {
+    "revision": "2760e0b45d3668f916ea",
+    "url": "/portfolio/static/css/main.14a3aa2f.chunk.css"
+  },
+  {
+    "revision": "fb1ea271bb96dffb23a38a7d0784c9cb",
+    "url": "/portfolio/index.html"
+  }
+];
